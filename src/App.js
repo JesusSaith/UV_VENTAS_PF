c9531ee194180b871b02af432f2850365339d4a7
JesusSaith <yisuscond@gmail.com>
2023-05-29 06:29:44 -0600

    nuevo

--- src/App.js
+++ src/App.js
@@ -60,6 +60,8 @@ class App extends Component {
 
 
     );
+
+
   }
 }
 
--- src/components/reacciones.component.js
+++ src/components/reacciones.component.js
@@ -2,7 +2,22 @@ import React, { useState, useEffect } from "react";
 import "../styles/reaccion.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faThumbsUp, faHeart, faAngry } from '@fortawesome/free-solid-svg-icons'
+import KafkaService from "../services/kafka.service";
 
+
+
+function saveLike(e, status) {
+  
+  let data = {
+    id: 0,
+    status: status
+  };
+
+  console.log(JSON.stringify(data));
+
+  KafkaService.reaction("i-love-adsoftsito");
+  e.preventDefault();
+}
 
 function Reactions() {
     const [reaction, setReaction] = useState(null);
@@ -35,7 +50,13 @@ function Reactions() {
         </button>
         <button
             className={reaction === "love" ? "active" : ""}
-            onClick={() => handleReactionClick("love")}
+            onClick={(e) => {
+              handleReactionClick("love")
+              e.preventDefault();
+                    saveLike(e, 1)
+          }
+        }
+              
         >
           <FontAwesomeIcon icon={faHeart} /> Me encanta
         </button>
@@ -49,10 +70,17 @@ function Reactions() {
         <p>Likes: {reactionsCount.like}
         Me encanta: {reactionsCount.love}
         Me enoja: {reactionsCount.angry}</p>
+
+       
+
+
+
+
       </div>
     );
   }
-  
+
+
   export default Reactions;
 
 
