dd7dac96a415de6ca8c5abb8e3ec75275760939f
JesusSaith <yisuscond@gmail.com>
2023-05-29 06:37:21 -0600

    nuevo

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
-const cors = require ('cors')
-const express = require ('express')
-const {kafka} = require ('kafkajs')
-
-const kafka = new kafka({
-  clientId: 'my app',
-  brokers: [
-    'my-kafka-0.my-kafka-headless.jesussaith.svc.cluster.local:9092'
-  ]
-  
-
-})
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
